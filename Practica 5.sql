9db997ee5816a2388cd3b061f8cdd2991336a093
JuanVillarreal99 <34010157+JuanVillarreal99@users.noreply.github.com>
2019-05-03 15:21:05 -0500

    Add files via upload

---
+++ Practica 5.sql
@@ -0,0 +1,331 @@
+use citas
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('1','esteban','2334566');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('23','Juan Alberto','23345226');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('434','Juan Villarreal','3984455');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('454','Sofia Alvarez','4557906');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('986','Estefania Alonso','8497622');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('777','Samuel del luque','834758');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('6676','Ana Luisa','876540');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('985676','Paloma Dominguez','8478997622');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('864','Alan Monrreal','847097622');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('986','Estefania Alonso','8497622');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('9838','Jose Alonso','7364795');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('7336','Alfonso Medina','7367489');
+
+INSERT INTO medicos (id,nombre,cedulaprofesional) 
+VALUES ('986','Estefania Alonso','8497622');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('344','Alameda','sexta #109 Residencial alameda');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('3455','Consultorio Villarreal','novena #205 obreras sur');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('566','Consultorio vida','Jose #505 guadalupe');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('7755','Consultorio 10','decima #2 mayo');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('656','Consultorio 04','mol #29 anahuac');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('777','salud saludable','primera de mayo #29 anahuac norte');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('6568','Consultorio 04','mol #29 anahuac');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('6778','Consultorio 999','89 #29 anahuac sur');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('656','Consultorio 04','mol #29 anahuac');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('656','SALUD','Barragan #363 los alamos');
+
+INSERT INTO consultorio (id,nombre,ubicacion) 
+VALUES ('656','UP','7 #777 recidencial alameda');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('33','Javier','8122102755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('34','Juan','8122202755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('43','Alan','8145902755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('33','Javier','8122102755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('93','Martin','8122108753','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('2','Alonso','8132102755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('7','Alex','8132202451','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('83','Alonso','445456567','indefinido');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('2','Alonso','8132102755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('84','Axel','813218837','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('22','Saul','8132333755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('3334','Ramiro','8132108963','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('780','Javier','8132102755','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('10','Juan Villarreal','8122102744','masculino');
+
+INSERT INTO paciente(id,nombre,telefono,sexo) 
+VALUES ('96347','All','8132102755','masculino');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('1','Daniela','42');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('87','Ana','28');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('2','Cecilia','20');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('9','Arleth','36');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('863','Carla','26');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('6','Mariana','42');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('872','Evelin','874');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('782','Daniela','42');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('11','Estefania','25');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('23','Javiera','22');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('267','Monica','49');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('176','Esmeralda','32');
+
+INSERT INTO secretaria(id,nombre,edad) 
+VALUES ('1431','Diana','42');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('1','sexta #98 recidencial anahuac');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('47','novena #31 recidencial anahuac tercer sector');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('143','sexta #23 guadalupe');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('33','ocho #6 obreras');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('344','33 #645 Alamos');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('56','Juarez #208b nueva rosita');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('444','Diana #56 Otilio');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('56','sexta #574 Las flores');
+
+INSERT INTO sitio(id,direccion) 
+VALUES ('566','revolucion #6 Los bosques');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('1','Javier','UANL');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('6','Alomar','UAC');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('8','Alonso','UANL');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('9','Martin','UANL');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('12','Juan Alonso','UNAM');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('2','Javier','TEC DE MONTERREY');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('566','Alonso Medina','UANL');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('65','Jose','UAC');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('197','Derek','harvard');
+
+INSERT INTO especialidad(id,nombre,universidad) 
+VALUES ('4312','Stefan','harvard');
+
+update medicos set nombre = 'Carlos'
+where id = 23
+
+update medicos set nombre = 'Martin'
+where id = 1
+
+update medicos set cedulaprofesional = '12345678'
+where id = 777
+
+update paciente set nombre = 'Sergio'
+where id = 34
+
+update paciente set teleefono = '76732234'
+where id = 84
+
+update consultorio set nombre = 'Salud'
+where id = 344
+
+update especialidad set nombre = 'Carlos'
+where id = 23
+
+update sitio set direccion = 'Juarez #208b nueva rosita'
+where id = 56
+
+update secretaria set edad = '32'
+where id = 2
+
+update secretaria set nombre = 'Alondra Villanueva'
+where id = 23
+
+delete from medicos
+where id = 8732
+
+delete from paciente
+where id = 731
+
+delete from especialidad
+where id = 91
+
+delete from secretaria
+where id = 8723
+
+delete from secretaria
+where id = 810
+
+delete from sitio
+where id = 8371
+
+delete from sitio
+where id = 816
+
+delete from consultorio
+where id = 8378
+
+delete from consultorio
+where id = 64
+
+delete from especialidad
+where id = 863
+
+select * from consultorio
+
+select * from medicos
+
+select * from especialidad
+
+select * from secretaria
+
+select * from paciente
+
+select * from sitio
+
+select * from signos_vitales
+
+select * from historial_medico
+
+select * from citas
+
+select * from especialidad_medico
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
